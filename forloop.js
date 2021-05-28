42b819cda91f1eba2468a6744fa7bfac938e13c9
Harsh Mittal <harshmittal1750@gmail.com>
2021-05-28 12:42:23 +0530

    forLoop

--- forloop.js
+++ forloop.js
@@ -1,1 +1,5 @@
-// will start tomorrow
+const states = ["haryana", "rajasthan", "punjab", 12332, "delhi"];
+for (let i = 0; i < states.length; i++) {
+  if (typeof states[i] !== "string") break;
+  console.log(states[i]);
+}
